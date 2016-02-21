251e9567098ae1de974050eb25b5b6520f514e00
hugeterry <xuxt-314@qq.com>
2016-02-22 02:17:32 +0800

    新建application

---
+++ app/src/main/java/cn/hugeterry/coderfun/CoderfunApplication.java
@@ -0,0 +1,19 @@
+package cn.hugeterry.coderfun;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ * Date: 16/2/22 02:04
+ */
+public class CoderfunApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+        FlowManager.init(this);
+    }
+}
