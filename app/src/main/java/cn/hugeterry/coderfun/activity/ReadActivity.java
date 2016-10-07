5d28abd9bf53d4eeca52d462f07d301f4ac3364e
HugeTerry <xuxt-314@qq.com>
2016-10-07 22:45:30 +0800

    整理内容

--- app/src/main/java/cn/hugeterry/coderfun/activity/ReadActivity.java
+++ app/src/main/java/cn/hugeterry/coderfun/activity/ReadActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.umeng.analytics.MobclickAgent;
